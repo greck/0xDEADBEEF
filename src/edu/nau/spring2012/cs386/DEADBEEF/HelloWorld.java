6617546bc3223db0f3ea42a7b24706cba8403770
greck cannon <greck@nau.edu>
2012-01-31 16:03:17 -0700

    No project is complete without a Hello, World app.

---
+++ src/edu/nau/spring2012/cs386/DEADBEEF/HelloWorld.java
@@ -0,0 +1,15 @@
+package edu.nau.spring2012.cs386.DEADBEEF;
+
+import lejos.nxt.*;
+
+public class HelloWorld {
+
+	    public static void main(String[] args) {
+	        System.out.println("NAU");
+	        System.out.println("Spring 2012");
+	        System.out.println("CS 386");
+	        System.out.println("Team 0xDEADBEEF");
+	        Button.waitForPress();
+	    }
+
+}
