1c8073a28715b8b1e1d5c36751e74f26553ad8b1
greck cannon <greck@nau.edu>
2012-01-31 18:27:16 -0700

    Iteration 1 of HW1 provides simple start/stop control of Motor A in response to Touch sensor input on S1.

---
+++ src/edu/nau/spring2012/cs386/DEADBEEF/DetectorListeners.java
@@ -0,0 +1,22 @@
+package edu.nau.spring2012.cs386.DEADBEEF;
+
+import lejos.nxt.Motor;
+import lejos.robotics.objectdetection.*;
+
+public class DetectorListeners implements FeatureListener {
+	
+	public void featureDetected(Feature feature, FeatureDetector detector) {
+
+		if(!Motor.A.isMoving()) {
+
+			Motor.A.forward();
+				
+		} else {
+
+			Motor.A.stop();
+
+		}
+
+	}
+		
+}
---
+++ src/edu/nau/spring2012/cs386/DEADBEEF/HW1.java
@@ -0,0 +1,21 @@
+package edu.nau.spring2012.cs386.DEADBEEF;
+
+import lejos.nxt.*;
+import lejos.robotics.*;
+import lejos.robotics.objectdetection.*;
+
+public class HW1 {
+
+	public static void main(String[] args) {
+
+		DetectorListeners detectorListener = new DetectorListeners();
+		
+		Touch touch = new TouchSensor(SensorPort.S1);
+		FeatureDetector startStop = new TouchFeatureDetector(touch);
+		startStop.addListener(detectorListener);
+
+		while(true);
+		
+	}
+		
+}
