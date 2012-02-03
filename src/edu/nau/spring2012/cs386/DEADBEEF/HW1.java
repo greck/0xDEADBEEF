61c8030824189002780cfa68ddae8ab31194de43
greck cannon <greck@nau.edu>
2012-02-02 17:04:28 -0700

    Emptied main().

--- src/edu/nau/spring2012/cs386/DEADBEEF/HW1.java
+++ src/edu/nau/spring2012/cs386/DEADBEEF/HW1.java
@@ -8,14 +8,16 @@ public class HW1 {
 
 	public static void main(String[] args) {
 
-		DetectorListeners detectorListener = new DetectorListeners();
+//		DetectorListeners detectorListener = new DetectorListeners();
+//
+//		Touch touch = new TouchSensor(SensorPort.S1);
+//		FeatureDetector startStop = new TouchFeatureDetector(touch);
+//		startStop.addListener(detectorListener);
+//
+//		Button.ENTER.waitForPressAndRelease();
 
-		Touch touch = new TouchSensor(SensorPort.S1);
-		FeatureDetector startStop = new TouchFeatureDetector(touch);
-		startStop.addListener(detectorListener);
-
-		Button.ENTER.waitForPressAndRelease();
-
+		
+		
 	}
 
 }
