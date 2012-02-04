684192c7819e5d2c97d3fc30beeff954b343a27a
greck cannon <greck@unpunk.com>
2012-02-04 08:06:48 -0700

    Cleaning up my development branch.

--- src/edu/nau/spring2012/cs386/DEADBEEF/HW1.java
+++ src/edu/nau/spring2012/cs386/DEADBEEF/HW1.java
@@ -1,9 +1,5 @@
 package edu.nau.spring2012.cs386.DEADBEEF;
 
-import lejos.nxt.*;
-import lejos.robotics.*;
-import lejos.robotics.objectdetection.*;
-
 public class HW1 {
 
 	public static void main(String[] args) {
