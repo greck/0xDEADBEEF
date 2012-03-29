5193139fe1b2d7203b6c2b1e6936ed82a1fc6212
Charles Charles <cc774@gmail.com>
2012-03-29 00:37:34 -0700

    CheckColor with close claw

--- src/edu/nau/spring2012/cs386/DEADBEEF/CheckColor.java
+++ src/edu/nau/spring2012/cs386/DEADBEEF/CheckColor.java
@@ -1,9 +1,20 @@
 package edu.nau.spring2012.cs386.DEADBEEF;
 
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+
 public class CheckColor implements Recipe {
 
 	public boolean execute() {
-
+		
+		// Code to close the claw.
+		Motor.C.rotate(80);
+		Button.ENTER.waitForPressAndRelease();
+		
+		// ----- CODE TO OPEN CLAW:
+		// Motor.C.rotate(-80);
+		// Button.ENTER.waitForPressAndRelease();
+		
 		if ( RobotState.colorObj.getRed() > RobotState.colorObj.getBlue() ) {
 			
 			return true;
