2f47f82663a145429f47c3c9af3620dd4c67bd10
greck cannon <greck@unpunk.com>
2012-03-29 00:44:10 -0700

    Merged claw opening from Chavez's dev branch.

--- src/edu/nau/spring2012/cs386/DEADBEEF/CheckColor.java
+++ src/edu/nau/spring2012/cs386/DEADBEEF/CheckColor.java
@@ -1,9 +1,18 @@
 package edu.nau.spring2012.cs386.DEADBEEF;
 
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+
 public class CheckColor implements Recipe {
 
 	public boolean execute() {
-
+		
+		// Code to close the claw.
+		Motor.C.rotate(80);
+		
+		// ----- CODE TO OPEN CLAW:
+		// Motor.C.rotate(-80);
+		
 		if ( RobotState.colorObj.getRed() > RobotState.colorObj.getBlue() ) {
 			
 			return true;
