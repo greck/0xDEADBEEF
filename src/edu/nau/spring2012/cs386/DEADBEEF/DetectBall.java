6e1776db0880bab21edb5a2ed6c68e94cf1b79e2
greck cannon <greck@unpunk.com>
2012-04-12 12:06:27 -0700

    Moved claw closing from CheckColor to DetectBall so that the polled color data will be correct when we enter a CheckColor state.

--- src/edu/nau/spring2012/cs386/DEADBEEF/DetectBall.java
+++ src/edu/nau/spring2012/cs386/DEADBEEF/DetectBall.java
@@ -1,5 +1,7 @@
 package edu.nau.spring2012.cs386.DEADBEEF;
 
+import lejos.nxt.Motor;
+
 public class DetectBall implements Recipe {
 
 	public boolean execute() {
@@ -7,6 +9,11 @@ public class DetectBall implements Recipe {
 		if ( RobotState.range < 25 ) {
 
 			RobotState.pilot.travel(20,false);
+
+			// Code to close the claw.
+			//
+			Motor.C.rotate(80);
+			
 			return true;
 
 		} else {
